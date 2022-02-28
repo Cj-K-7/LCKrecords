4da8d03362006c7b55408d5c764ccec20a204f4c
Cj-K-7 <kllt7059@gmail.com>
2022-02-28 11:35:22 +0900

    init

---
+++ src/style/theme.ts
@@ -0,0 +1,7 @@
+import { DefaultTheme } from "styled-components";
+
+export const theme: DefaultTheme = {
+    backgroundColor : 'rgb(31, 26, 46)',
+    textColor : 'rgb(36, 36, 36)',
+    textHover : 'rgb(0, 0, 0)',
+}
