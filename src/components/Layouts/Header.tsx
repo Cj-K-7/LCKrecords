8c600888fac692bd43978bdfe9cd8a36374ff67b
Cj-K-7 <kllt7059@gmail.com>
2022-04-24 17:38:19 +0900

    spining Logo

--- src/components/Layouts/Header.tsx
+++ src/components/Layouts/Header.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import UserInfo from "./UserInfo";
 import { auth } from "../../firebase";
 import { Link, useNavigate } from "react-router-dom";
-import { LCK } from "../SVGs";
+import Logo from "./Logo";
 
 const HeaderBox = styled.div`
   width: 100vw;
@@ -26,12 +26,13 @@ const Menu = styled.div`
     padding: 0px 20px;
   }
   & a:hover {
-    color : ${props=> props.theme.textHover};
+    color: ${(props) => props.theme.textHover};
   }
 `;
 const Info = styled.div`
   display: flex;
   align-items: center;
+  padding-right: 80px;
   svg path {
     fill: ${(props) => props.theme.textColor};
   }
@@ -74,12 +75,12 @@ const Sign = styled.h1`
   &:hover {
     text-shadow: 0 0 3px white;
   }
-`
+`;
 
 function Header() {
   const [isClicked, setIsClicked] = useState(false);
   const navigation = useNavigate();
-  
+
   //season 에 따라 Routing 위치 바뀌게끔 설계 추후 추가 필요.
 
   return (
@@ -102,8 +103,10 @@ function Header() {
                   <path d="M224 256c70.7 0 128-57.31 128-128s-57.3-128-128-128C153.3 0 96 57.31 96 128S153.3 256 224 256zM274.7 304H173.3C77.61 304 0 381.6 0 477.3c0 19.14 15.52 34.67 34.66 34.67h378.7C432.5 512 448 496.5 448 477.3C448 381.6 370.4 304 274.7 304z" />
                 </User>
               </Cover>
-            ) : <Sign onClick={()=>navigation('/auth')}> Sign In </Sign>}
-            <LCK/>
+            ) : (
+              <Sign onClick={() => navigation("/auth")}> Sign In </Sign>
+              )}
+              <Logo />
           </Info>
         </Margin>
       </HeaderBox>
---
+++ src/components/Layouts/Logo.tsx
@@ -0,0 +1,69 @@
+import styled from "styled-components";
+import { LCK } from "../SVGs";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: 2.5s ease-in-out;
+  transform-style: preserve-3d;
+  animation: rotating 6s infinite;
+  @keyframes rotating {
+      0%{
+          transform: rotateX(0deg);
+      }
+      100%{
+          transform: rotateX(360deg);
+      }
+  }
+`;
+const Box = styled.div`
+  transform-style: preserve-3d;
+  position: relative;
+  transform: rotateY(20deg) rotateX(28deg);
+  &::before{
+      content: '';
+      position: absolute;
+      width: 60px;
+      height: 60px;
+      background-color: ${props=>props.theme.backgroundColor2};
+      transform: rotateY(90deg) translateZ(-29px);
+  }
+  & svg {
+    position: absolute;
+    width: 50px;
+    height: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color : ${props=>props.theme.textColor};
+    fill: ${props=>props.theme.textColor};
+    transform-style: preserve-3d;
+    background-color: ${props=>props.theme.backgroundColor2};
+    transform: translateZ(30px);
+    &:nth-child(2){
+        transform: rotateX(90deg) translateZ(30px);
+    }
+    &:nth-child(3){
+        transform: rotateX(180deg) translateZ(30px);
+    }
+    &:nth-child(4){
+        transform: rotateX(270deg) translateZ(30px);
+    }
+  }
+`;
+
+function Logo() {
+  return (
+    <Container>
+      <Box>
+            <LCK/>
+            <LCK/>
+            <LCK/>
+            <LCK/>
+      </Box>
+    </Container>
+  );
+}
+
+export default Logo;
