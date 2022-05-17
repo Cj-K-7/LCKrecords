7c4331e14a8231e987dcd89365d31259744e9abe
CJ-K-7 <kllt7059@gmail.com>
2022-05-17 22:22:26 +0900

    firebase message test

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,13 +1,15 @@
 import {initializeApp} from "firebase/app";
 import {getAuth} from "firebase/auth"
 import {doc, getFirestore} from 'firebase/firestore'
+import { getMessaging, getToken, onMessage } from 'firebase/messaging'
+import { onBackgroundMessage } from "firebase/messaging/sw";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId0: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
   measurementId: process.env.REACT_APP_MEASUERMENT_ID,
 };
@@ -20,3 +22,17 @@ export const database = getFirestore(app);
 export const auth = getAuth();
 
 export const springSplitDB = doc(database, "DB", "SpringSplit");
+
+export const messaging = getMessaging(app)
+
+getToken(messaging, { vapidKey: process.env.REACT_APP_KEY_PAIR }).then((currentToken)=>{
+  if(currentToken){
+    console.log(currentToken);
+    alert(currentToken);
+  } else {
+    console.log('no registration token available');
+  }
+}).catch(err=>{ console.error(err)});
+
+// onMessage(messaging, (payload)=>{alert(payload)})
+
